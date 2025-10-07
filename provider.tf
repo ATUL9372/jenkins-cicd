3484082262216a3f5e90396766f6d620d6c0d451
ATUL9372 <atulyadav9372@gmail.com>
2025-10-07 14:34:24 +0530

    Added provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = ""
+  access_key = ""
+  secret_key = ""
+}
