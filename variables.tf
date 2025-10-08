882f0f49894f9f15bbdfdf2fd2f5f3a4263e215c
ATUL9372 <atulyadav9372@gmail.com>
2025-10-08 15:09:47 +0530

    update & add more variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
-// IAM Config
+// provider Config
 
 variable "region" {
   type = string
+  default = "ap-south-1"
+  description = "Default region is mumbai"
 }
 
 variable "access_key" {
@@ -12,3 +14,61 @@ variable "secret_key" {
   type = string
 }
 
+// AWS EC2 Instances Config
+
+variable "server_ami" {
+  type = string
+  default = "ami-00cd1596c62928384"
+  description = "This ami is ubuntu 24.04"
+}
+
+variable "server_type" {
+  type = string
+  default = "t2.micro"
+  description = "Instance config default is 
+  vCPU: 1 and RAM: 1"
+
+}
+
+variable "server_name"{
+  type = string
+  default = "demo_server_terra"
+  description = "Default server name"
+}
+
+variable "server_owner"{
+  type = string
+  default = "atul_terraform"
+
+}
+
+variable "server_can_be_deleted"{
+  type = string
+  default = "Ask Owner"
+
+}
+
+
+// storage config
+
+
+variable "volume_type"{
+  type = string
+  default = "gp2"
+}
+
+variable "volume_storage_size"{
+  type = string
+  default = "29"
+}
+
+variable "delete_on_termination"{
+  type = string
+  default = "true"
+}
+
+// AWS Key Pair
+
+variable "public_ssh_key"{
+  type = string
+}
